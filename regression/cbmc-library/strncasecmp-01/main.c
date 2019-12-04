f1b0e662ff039ee6b21fe7ca3e8b488444d48c8f
Mark R. Tuttle <mrtuttle@amazon.com>
2019-12-04 15:07:30 -0500

    Fix more clang-format issues on patch to strncmp(a,b,n) for n==0.

--- regression/cbmc-library/strncasecmp-01/main.c
+++ regression/cbmc-library/strncasecmp-01/main.c
@@ -7,10 +7,10 @@ int main()
   char b[] = "xyz";
   char A[] = "ABC";
   char B[] = "XYZ";
-  assert(strncasecmp(a,b,0) == 0);
-  assert(strncasecmp(A,B,0) == 0);
-  assert(strncasecmp(A,b,0) == 0);
-  assert(strncasecmp(A,b,2) == -1);
-  assert(strncasecmp(B,a,2) == 1);
+  assert(strncasecmp(a, b, 0) == 0);
+  assert(strncasecmp(A, B, 0) == 0);
+  assert(strncasecmp(A, b, 0) == 0);
+  assert(strncasecmp(A, b, 2) == -1);
+  assert(strncasecmp(B, a, 2) == 1);
   return 0;
 }
--- regression/cbmc-library/strncmp-01/main.c
+++ regression/cbmc-library/strncmp-01/main.c
@@ -8,8 +8,8 @@ int main()
 {
   char a[] = "abc";
   char b[] = "xyz";
-  assert(strncmp(a,b,0) == 0);
-  assert(strncmp(a,b,1) == -1);
-  assert(strncmp(b,a,1) == 1);
+  assert(strncmp(a, b, 0) == 0);
+  assert(strncmp(a, b, 1) == -1);
+  assert(strncmp(b, a, 1) == 1);
   return 0;
 }
