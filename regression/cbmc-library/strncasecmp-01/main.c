49746659589499d19ce9348c69ddbbc283af880f
Mark R. Tuttle <mrtuttle@amazon.com>
2019-12-04 14:07:26 -0500

    Improve patch to ensure strncmp(a,b,n) compares at most n characters.
    Formatted the strncmp patch to make clang-format happy, applied the
    same patch to strncasecmp, and added regression tests in
    regression/cbmc-library/strncmp-01 and regression/cbmc-library/strncasecmp-01

--- regression/cbmc-library/strncasecmp-01/main.c
+++ regression/cbmc-library/strncasecmp-01/main.c
@@ -1,9 +1,15 @@
 #include <assert.h>
 #include <string.h>
 
-int main()
-{
-  strncasecmp();
-  assert(0);
+int main() {
+  char a[] = "abc";
+  char b[] = "xyz";
+  char A[] = "ABC";
+  char B[] = "XYZ";
+  assert(strncasecmp(a,b,0) == 0);
+  assert(strncasecmp(A,B,0) == 0);
+  assert(strncasecmp(A,b,0) == 0);
+  assert(strncasecmp(A,b,2) == -1);
+  assert(strncasecmp(B,a,2) == 1);
   return 0;
 }
